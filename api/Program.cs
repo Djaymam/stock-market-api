fa30413e0c7ab85ada4abb6116cc6a2830987858
Jaylson Almeida <jaylson.almeida@luukal.com>
2024-10-17 20:31:52 -0100

    Setting up Database with Eframework

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using api.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDBContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
